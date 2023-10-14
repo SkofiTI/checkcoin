2e06289b92a0305d63c7e950003c539d8c6ae34b
SkofiTI <cactus.amster994@gmail.com>
2023-10-14 19:28:27 +0300

    feat: Add a banner with cryptocurrencies

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -26,9 +26,37 @@
         </ul>
     </div>
     <div class="content">
-        <img id="bitcoin-image" src="/img/Bitcoin.png" alt="Bitcoin">
-        <p>Monitor exchange rate changes live</p>
-        <img id="tether-image" src="/img/Tether.png" alt="Tether">
+        <div class="main-content">
+            <img id="bitcoin-image" src="/img/Bitcoin_Big.png" alt="Bitcoin_Big">
+            <p>Monitor exchange rate changes live</p>
+            <img id="tether-image" src="/img/Tether_Big.png" alt="Tether">
+        </div>
+        <div class="base-currency">
+            <div class="base-currency__item">
+                <img class="base-currency__image" src="/img/Bitcoin.png" alt="Bitcoin">
+                <p class="base-currency__name">Bitcoin</p>
+                <p class="base-currency__price">42134 $</p>
+                <p class="base-currency__percent">4.123%</p>
+            </div>
+            <div class="base-currency__item">
+                <img class="base-currency__image" src="/img/Tether.png" alt="Tether">
+                <p class="base-currency__name">Tether</p>
+                <p class="base-currency__price">42134 $</p>
+                <p class="base-currency__percent">4.123%</p>
+            </div>
+            <div class="base-currency__item">
+                <img class="base-currency__image" src="/img/Ethereum.png" alt="Ethereum ">
+                <p class="base-currency__name">Ethereum</p>
+                <p class="base-currency__price">42134 $</p>
+                <p class="base-currency__percent">4.123%</p>
+            </div>
+            <div class="base-currency__item">
+                <img class="base-currency__image" src="/img/Ripple.png" alt="Ripple ">
+                <p class="base-currency__name">Ripple</p>
+                <p class="base-currency__price">42134 $</p>
+                <p class="base-currency__percent">4.123%</p>
+            </div>
+        </div>
     </div>
 </body>
 </html>
